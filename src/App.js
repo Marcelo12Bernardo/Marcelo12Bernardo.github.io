7d0db2a618acfbc207298416827a18b94da207ac
Marcelo Bernardo <bernardomarcelo33@gmail.com>
2023-05-17 10:04:02 -0300

    feat: cria a rota 'sobre'

--- src/App.js
+++ src/App.js
@@ -1,11 +1,13 @@
 import { Route, Switch } from 'react-router-dom';
 import Home from "./Pages/Home";
+import Sobre from './Pages/Sobre';
 
 function App() {
   return (
     <main id="defoult">
       <Switch>
         <Route exact path="/" component={ Home } />
+        <Route exact path="/sobre" component={ Sobre } />
       </Switch>
     </main>
   );
---
+++ src/Pages/Sobre.js
@@ -0,0 +1,10 @@
+import React from "react";
+
+class Sobre extends React.Component {
+    render(){
+        return(
+            <h1>Sobre</h1>
+        );
+    }
+}
+export default Sobre;
