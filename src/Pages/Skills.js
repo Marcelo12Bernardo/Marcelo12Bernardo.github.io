9d7f69dd59f92efcb57b2d0b80040d98d4424b13
Marcelo Bernardo <bernardomarcelo33@gmail.com>
2023-05-29 12:41:50 -0300

    styles: estiliza tela de skills

--- src/Pages/Skills.js
+++ src/Pages/Skills.js
@@ -2,6 +2,7 @@ import React from "react";
 import Header from "../Components/Header";
 import { FaGithub, FaJsSquare, FaReact, FaGitAlt, FaCss3Alt, FaHtml5, FaLinux,
   FaJava,FaPython, FaPhp, FaDocker,FaBootstrap,FaNodeJs, FaAndroid, FaDatabase, } from "react-icons/fa";
+import {SiEslint, SiTailwindcss } from "react-icons/si";
 
 class Skills extends React.Component{
   render(){
@@ -9,10 +10,9 @@ class Skills extends React.Component{
     return(
       <div>
         <Header history={history}/>
-        <section>
-          <h1>Minhas Techs</h1>
-          <section>
-            <h1>Tecnologias<br/> Ja utilizadas</h1>
+          <h1 className="text-primary text-6xl font-bold text-center mt-20">Minhas Techs</h1>
+          <h1 className="text-secondary text-3xl font-bold text-center mt-24">Tecnologias de projetos anteriores</h1>
+          <section className="flex gap-8 text-6xl justify-center text-primary mt-12">
             <FaJava/>
             <FaPython/>
             <FaPhp/>
@@ -22,17 +22,19 @@ class Skills extends React.Component{
             <FaAndroid/>
             <FaDatabase/>
           </section>
-          <section>
-            <h1>Tecnologias<br/>Utilizadas no dia a dia</h1>
+          <h1 className="text-secondary text-3xl font-bold text-center mt-20">Tecnologias utilizadas no dia a dia</h1>
+          <section className="flex gap-8 text-6xl justify-center text-primary mt-12">
             <FaGithub/>
+            <FaGitAlt/>
             <FaJsSquare/>
             <FaReact/>
-            <FaGitAlt/>
+            <SiTailwindcss/>
+            <SiEslint/>
             <FaCss3Alt/>
             <FaHtml5/>
             <FaLinux/>
           </section>
-        </section>
+      
       </div>
     );
   }
