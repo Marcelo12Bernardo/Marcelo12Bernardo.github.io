579e93436b63d589948efc445f7b89de6a922726
Marcelo Bernardo <bernardomarcelo33@gmail.com>
2023-06-09 12:58:25 -0300

    build: adciona novas dependencias e corrige estilização

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,20 +1,17 @@
 /** @type {import('tailwindcss').Config} */
 module.exports = {
-  content: ["./src/**/*.{html,js}"],
-  theme: {
-    extend: {
-      colors:{
-        primary: '#445964',
-        secondary: '#263138',
-        terceary: '#DFDFDF',
-        backgb: '#FBFBFB',
-      },
-      maxHeight:{
-        contProjct: '35rem',
-      },
+    purge: ['./src/**/*.js', './src/**/*.jsx', './src/**/*.html'],
+    theme: {
+        colors:{
+            primary: '#445964',
+            secondary: '#263138',
+            terceary: '#DFDFDF',
+            backgb: '#FBFBFB',
+        },
+        maxHeight:{
+            contProjct: '35rem',
+        },
     },
-  },
-  plugins: [],
-}
-
-
+    plugins: [],
+};
+  
