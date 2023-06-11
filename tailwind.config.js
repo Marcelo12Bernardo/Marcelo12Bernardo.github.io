b4210d6d2c940579f024a542e3f38e4a7af251bd
Marcelo Bernardo <bernardomarcelo33@gmail.com>
2023-06-11 11:52:26 -0300

    home

--- tailwind.config.js
+++ tailwind.config.js
@@ -2,16 +2,28 @@
 module.exports = {
     purge: ['./src/**/*.js', './src/**/*.jsx', './src/**/*.html'],
     theme: {
-        colors:{
-            primary: '#445964',
-            secondary: '#263138',
-            terceary: '#DFDFDF',
-            backgb: '#FBFBFB',
-        },
-        maxHeight:{
-            contProjct: '35rem',
+        extend: {
+            colors: {
+                primary: '#445964',
+                secondary: '#263138',
+                terceary: '#DFDFDF',
+                backgb: '#FBFBFB',
+            },
+            maxHeight: {
+                contProjct: '35rem',
+            },
+            fontSize: {
+                '2xl': '24px',
+                '3xl': '30px',
+                '4xl': '36px',
+                '5xl': '48px',
+                '6xl': '60px',
+                '7xl': '72px',
+                '8xl': '96px',
+                '9xl': '128px',
+            },
         },
     },
     plugins: [],
-};
+};  
   
